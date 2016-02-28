a99473f1e13ca8a8243518affd44e6512bab1dc0
Ewong3 <eric3@ualberta.ca>
2016-02-28 14:20:49 -0700

    More framework

--- src/app/src/main/java/ca/ualberta/cs/technologic/NewUserActivity.java
+++ src/app/src/main/java/ca/ualberta/cs/technologic/NewUserActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -21,16 +22,31 @@ public class NewUserActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newuser);
 
-        // Set confirmUserButton click
+        existingUsers = getUsers();
+
+        // Set confirmUserButton click. Return Intent will be implemented along with LoginActivity
         Button confirmUserButton = (Button) findViewById(R.id.confirmNewUser);
         confirmUserButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // TODO: Check if the filled in information is correct. No missing information and valid Username
                 // TODO: Return to LoginActivity if correct, otherwise stay and give notice
+
+                getUserInput();
+                // Validate the username. Do we want to validate all other String fields?
+                if (validUsername(pendingUser.getUsername())) {
+                    // Add to existingUsers and return Intent
+
+                } else {
+                    // Error message. Username already taken
+                    Toast takenUser = Toast.makeText(getApplicationContext(),
+                            "Username is already taken", Toast.LENGTH_SHORT);
+                    takenUser.show();
+                }
             }
         });
 
+        // Set cancelUserButton click. Return Intent will be implemented along with LoginActivity
         Button cancelUserButton = (Button) findViewById(R.id.cancelNewUser);
         cancelUserButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,22 +56,28 @@ public class NewUserActivity extends Activity {
         });
     }
 
-    public void getUsers() {
-        // TODO: Load up or get the ArrayList of Users
+    /**
+     * gets the Users
+     * @return an ArrayList<User> of all User's who currently exist
+     */
+    public ArrayList<User> getUsers() {
+        // TODO: Load up or get the ArrayList of current Users
+        return null;
     }
 
-    public boolean checkUsername(String wantedUsername) {
+    /**
+     * Checks if the User's inputted username is unique
+     * @param wantedUsername a String
+     * @return true if the String is unique to the ArrayList, false if otherwise
+     */
+    public boolean validUsername(String wantedUsername) {
         // TODO: Check wantedUsername against existing Users
         return false;
     }
 
-    // Helper function
-    public String EditTextToString (EditText editText) {
-
-        return editText.getText().toString();
-
-    }
-
+    /**
+     * Gets the input text fields filled out by the User
+     */
     public void getUserInput() {
         // TODO: get the user's input from the EditText fields
         /*
@@ -74,7 +96,26 @@ public class NewUserActivity extends Activity {
         EditText ET_newPhoneNum = (EditText) findViewById(R.id.phoneNum);
         EditText ET_newAddress = (EditText) findViewById(R.id.address);
 
+        String newName = EditTextToString(ET_newName);
+        String newUserName = EditTextToString(ET_newUserName);
+        String newPassword = EditTextToString(ET_newPassword);
+        String newEmail = EditTextToString(ET_newEmail);
+        String newPhoneNum = EditTextToString(ET_newPhoneNum);
+        String newAddress = EditTextToString(ET_newAddress);
 
+        //TODO: Create a new user object. Depends on our constructor
+        //pendingUser = new User()
+
+    }
+
+
+    /**
+     * Helper function for getting the text as a string from an EditText object
+     * @param editText is an EditText object
+     * @return returns the string of the EditText object
+     */
+    public String EditTextToString (EditText editText) {
+        return editText.getText().toString();
     }
 
 }
