e546329fbd973a39d737bb20456d6d87c90c3604
Ewong3 <eric3@ualberta.ca>
2016-03-06 15:37:10 -0700

    User activity changes

--- src/app/src/main/java/ca/ualberta/cs/technologic/Activities/NewUser.java
+++ src/app/src/main/java/ca/ualberta/cs/technologic/Activities/NewUser.java
@@ -3,6 +3,7 @@ package ca.ualberta.cs.technologic.Activities;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,7 +21,8 @@ public class NewUser extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        // Do not give access to Users who have not signed up
+        // getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -28,8 +30,8 @@ public class NewUser extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.newuser);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-                existingUsers = getUsers();
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        existingUsers = getUsers();
 
         // Set confirmUserButton click. Return Intent will be implemented along with LoginActivity
         Button confirmUserButton = (Button) findViewById(R.id.userSubmit);
@@ -40,9 +42,10 @@ public class NewUser extends ActionBarActivity {
                 // TODO: Return to LoginActivity if correct, otherwise stay and give notice
 
                 pendingUser = getUserInput();
-                // Validate the username. Do we want to validate all other String fields?
+
+                //TODO: Validate the username. Do we want to validate all other String fields?
                 if (validUsername(pendingUser.getUsername())) {
-                    // Add to existingUsers and return Intent
+                    // Add to existingUsers, save and return Intent
 
                 } else {
                     // Error message. Username already taken
@@ -50,6 +53,7 @@ public class NewUser extends ActionBarActivity {
                             "Username is already taken", Toast.LENGTH_SHORT);
                     takenUser.show();
                 }
+
             }
         });
 
@@ -90,26 +94,31 @@ public class NewUser extends ActionBarActivity {
      */
     public User getUserInput() {
         // TODO: get the user's input from the EditText fields
-        // Get all EditText views
-        EditText ET_newName = (EditText) findViewById(R.id.userUsername);
-        EditText ET_newUserName = (EditText) findViewById(R.id.userName);
+        // Get all EditText view inputs
+        EditText ET_newUserName = (EditText) findViewById(R.id.userUsername);
         EditText ET_newPassword = (EditText) findViewById(R.id.userPassword);
+        EditText ET_newName = (EditText) findViewById(R.id.userName);
         EditText ET_newEmail = (EditText) findViewById(R.id.userEmail);
         EditText ET_newPhoneNum = (EditText) findViewById(R.id.userPhone);
         EditText ET_newAddress = (EditText) findViewById(R.id.userAddress);
 
         // Get all Strings from EditText views
-        String newName = EditTextToString(ET_newName);
         String newUserName = EditTextToString(ET_newUserName);
         String newPassword = EditTextToString(ET_newPassword);
+        String newName = EditTextToString(ET_newName);
         String newEmail = EditTextToString(ET_newEmail);
         String newPhoneNum = EditTextToString(ET_newPhoneNum);
         String newAddress = EditTextToString(ET_newAddress);
 
-        //TODO: Create a new user object. Depends on our constructor
-        //User inputUser = new User();
-        //return inputUser;
-        return null;
+        // Create new user with the input information
+        User inputUser = new User(newUserName, newPassword);
+
+        inputUser.setName(newName);
+        inputUser.setEmail(newEmail);
+        inputUser.setPhone(newPhoneNum);
+        inputUser.setAddress(newAddress);
+
+        return inputUser;
     }
 
 
